1119b5c921820d15987040ece5f162cd015e3024
Xand Meaden <xand.meaden@kcl.ac.uk>
2025-06-24 10:04:54 +0100

    fix: insert options in the right place, not between -p and name
    Info: Checking for compose project xnat
    Debug: Executing: '/usr/bin/docker compose -f /etc/xnat_docker_compose.yaml -p --project-directory /opt/xnat-docker-compose xnat config'
    Error: /Stage[main]/Role::Tre_app::Common::Pharos_ai/Docker_compose[xnat]: Could not evaluate: (<unknown>): could not find expected ':' while scanning a simple key at line 3 column 1
    Wrapped exception:
    (<unknown>): could not find expected ':' while scanning a simple key at line 3 column 1

--- lib/puppet/provider/docker_compose/ruby.rb
+++ lib/puppet/provider/docker_compose/ruby.rb
@@ -26,7 +26,7 @@ Puppet::Type.type(:docker_compose).provide(:ruby) do
     set_tmpdir
 
     # get merged config using docker-compose config
-    args = ['compose', compose_files, '-p', name, 'config'].insert(3, resource[:options]).compact
+    args = ['compose', compose_files, '-p', name, 'config'].insert(4, resource[:options]).compact
     compose_output = Puppet::Util::Yaml.safe_load(execute([command(:docker)] + args, combine: false), [Symbol])
 
     containers = docker([
@@ -74,21 +74,21 @@ Puppet::Type.type(:docker_compose).provide(:ruby) do
 
   def create
     Puppet.info("Running compose project #{name}")
-    args = ['compose', compose_files, '-p', name, 'up', '-d', '--remove-orphans'].insert(3, resource[:options]).insert(5, resource[:up_args]).compact
+    args = ['compose', compose_files, '-p', name, 'up', '-d', '--remove-orphans'].insert(4, resource[:options]).insert(5, resource[:up_args]).compact
     docker(args)
     return unless resource[:scale]
 
     instructions = resource[:scale].map { |k, v| "#{k}=#{v}" }
     Puppet.info("Scaling compose project #{name}: #{instructions.join(' ')}")
-    args = ['compose', compose_files, '-p', name, 'scale'].insert(3, resource[:options]).compact + instructions
+    args = ['compose', compose_files, '-p', name, 'scale'].insert(4, resource[:options]).compact + instructions
     docker(args)
   end
 
   def destroy
     Puppet.info("Removing all containers for compose project #{name}")
-    kill_args = ['compose', compose_files, '-p', name, 'kill'].insert(3, resource[:options]).compact
+    kill_args = ['compose', compose_files, '-p', name, 'kill'].insert(4, resource[:options]).compact
     docker(kill_args)
-    rm_args = ['compose', compose_files, '-p', name, 'rm', '--force', '-v'].insert(3, resource[:options]).compact
+    rm_args = ['compose', compose_files, '-p', name, 'rm', '--force', '-v'].insert(4, resource[:options]).compact
     docker(rm_args)
   end
 
@@ -96,9 +96,9 @@ Puppet::Type.type(:docker_compose).provide(:ruby) do
     return unless exists?
 
     Puppet.info("Rebuilding and Restarting all containers for compose project #{name}")
-    kill_args = ['compose', compose_files, '-p', name, 'kill'].insert(3, resource[:options]).compact
+    kill_args = ['compose', compose_files, '-p', name, 'kill'].insert(4, resource[:options]).compact
     docker(kill_args)
-    build_args = ['compose', compose_files, '-p', name, 'build'].insert(3, resource[:options]).compact
+    build_args = ['compose', compose_files, '-p', name, 'build'].insert(4, resource[:options]).compact
     docker(build_args)
     create
   end
