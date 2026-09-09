ddd023ee53eaedea061fbe849649faf589363999
Skylar Kelty <skylar.kelty@kcl.ac.uk>
2026-09-09 10:21:29 +0100

    Fix Puppet 8 compatibility

--- lib/puppet/provider/docker_stack/ruby.rb
+++ lib/puppet/provider/docker_stack/ruby.rb
@@ -14,7 +14,7 @@ Puppet::Type.type(:docker_stack).provide(:ruby) do
     stack_services = {}
     stack_containers = []
     resource[:compose_files].each do |file|
-      compose_file = YAML.safe_load(File.read(file), [], [], true)
+      compose_file = YAML.safe_load(File.read(file), permitted_classes: [], permitted_symbols: [], aliases: true)
       # rubocop:disable Style/StringLiterals
       containers = docker([
                             'ps',
